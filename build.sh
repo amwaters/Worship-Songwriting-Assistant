ac047fa6996373d61faa364265a19828b87d9c76
Aaron Waters <aaron@aaronwaters.net>
2024-01-23 15:10:51 +1100

    Tidying up files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-pushd "$DIR" > /dev/null ; trap "popd > /dev/null" EXIT
-
-# pack data files into a tarball
-tar -cf data/packages.tar packages
